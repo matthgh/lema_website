bc3a6ecb71dc0b8f86f52e0c9ca55a7f2bd97120
Matteo Haghel <matteo.haghel@gmail.com>
2024-06-21 17:58:28 +0200

    modified:   lema/lema/settings.py

--- lema/lema/settings.py
+++ lema/lema/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    #   custom apps
+    # custom apps
     "accounts",
     "client",
     "administration",
