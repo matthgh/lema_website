db3fa58722f396bb589110003582858bdd2dd0d5
Matteo Haghel <matteo.haghel@gmail.com>
2024-07-04 14:01:18 +0200

    main

--- lema/lema/settings.py
+++ lema/lema/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure--bq-**%gohnns9c3c9yn@j0_u^i8g)472@e81jilxoj@8onos7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.20.10.5']
+ALLOWED_HOSTS = ["172.20.10.5", ".vercel.app"]
 
 
 # Application definition
--- lema/lema/wsgi.py
+++ lema/lema/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lema.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lema.settings")
 
 application = get_wsgi_application()
+
+
+app = application
