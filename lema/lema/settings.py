2bdcf47d3956405015788519a7a86296add37a68
Matteo Haghel <matteo.haghel@gmail.com>
2024-07-09 19:38:51 +0200

    main

--- lema/lema/settings.py
+++ lema/lema/settings.py
@@ -143,10 +143,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-MEDIA_URL = "images/"
-STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -168,5 +164,3 @@ TAILWIND_APP_NAME = "frontend"
 from shutil import which
 
 NPM_BIN_PATH = which("npm")
-
-DISABLE_COLLECTSTATIC=1
